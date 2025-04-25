237478b7c687b434d1abf58e6b583fa83ade06aa
Sara <sarahpiri992@gmail.com>
2025-04-25 18:13:20 +0200

    improve add to cart

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   productCards.forEach((card) => {
     const btn = card.querySelector(".Waffle-btn");
     const name = card.querySelector("p").innerText.trim();
+    const productImage = card.querySelector(".Waffle");
     const priceText = card
       .querySelector("h4")
       .innerText.replace("$", "")
@@ -27,9 +28,16 @@ document.addEventListener("DOMContentLoaded", () => {
         updateCart();
       }
 
+      // add border to pictures after selecting
+      productImage.classList.add("active-border");
+
       function createCounter(btn, name) {
         const counter = document.createElement("div");
         counter.classList.add("counter");
+
+        // add border to pictures
+        const productImage = btn.closest(".card-1").querySelector("img");
+        productImage.classList.add("active-border");
 
         const minus = document.createElement("img");
         minus.src = "./assets/images/icon-decrement-quantity.svg";
@@ -41,15 +49,24 @@ document.addEventListener("DOMContentLoaded", () => {
         plus.alt = "plus";
         plus.classList.add("btn-plus");
 
+        const minusBtn = document.createElement("div");
+        minusBtn.classList.add("minusBtn");
+
+        const plusBtn  = document.createElement("div");
+        plusBtn.classList.add("plusBtn")
+
         const countText = document.createElement("span");
         countText.textContent = cartItems[name].quantity;
 
         btn.replaceChildren();
-        btn.appendChild(minus);
+        btn.appendChild(minusBtn);
         btn.appendChild(countText);
-        btn.appendChild(plus);
+        btn.appendChild(plusBtn);
+        plusBtn.appendChild(plus);
+        minusBtn.appendChild(minus);
         btn.classList.add("added");
 
+        // handle minus btn
         minus.addEventListener("click", () => {
           if (!cartItems[name]) return;
           cartItems[name].quantity--;
@@ -57,36 +74,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
           if (newQty === 0) {
             delete cartItems[name];
-            console.log(cartItems[name], newQty);
 
             btn.innerHTML = `<img src='./assets/images/icon-add-to-cart.svg' alt='' />Add to Cart`;
             btn.classList.remove("added");
-            btn.addEventListener(
-              "click",
-              () => {
-                cartItems[name] = {
-                  quantity: 1,
-                  price: cartItems[name]?.price || 0,
-                };
-                createCounter(btn, name);
-                updateCart();
-              },
-              { once: true }
-            ); // فقط یک بار اجرا بشه
+           
+            // remove boredre from picture 
+            productImage.classList.remove("active-border");
+            // add to cart back to default style
+            const newBtn = btn.cloneNode(true);
+            btn.replaceWith(newBtn);
+            newBtn.addEventListener("click", () => {
+              cartItems[name] = { quantity: 1, price, imageSrc };
+              createCounter(newBtn, name);
+              updateCart();
+            });
           } else {
             countText.textContent = newQty;
           }
-
           updateCart();
         });
 
-        plus.addEventListener("click", () => {
+        // handle plus btn
+        plus.addEventListener("click", (e) => {
+          e.stopPropagation();
           cartItems[name].quantity++;
           countText.textContent = cartItems[name].quantity;
           updateCart();
         });
       }
-
       updateCart();
     });
   });
@@ -178,11 +193,11 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   startNewOrder.addEventListener("click", () => {
-    // پاک کردن سبد خرید و بستن مدال
-    for (const name in cartItems) {
-      delete cartItems[name];
-    }
-    orderConfirmed.classList.add("hidden");
-    updateCart();
+    // for (const name in cartItems) {
+    //   delete cartItems[name];
+    // }
+    // orderConfirmed.classList.add("hidden");
+    // updateCart();
+    location.reload();
   });
 });
